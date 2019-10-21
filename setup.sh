61db60188bbe34ca1c4c5cd249226f5c19181e53
CleanestMink126 <gns126@gmail.com>
2019-10-20 21:39:19 -0400

    add atom

--- setup.sh
+++ setup.sh
@@ -6,10 +6,14 @@ echo "Hello $NAME!"
 cp ./.bash_aliases ~/
 sudo apt install python-pip
 sudo apt install git
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+sudo apt-get install python3
 pip install numpy
 pip install tensorflow
 pip install --upgrade tensorflow-gpu
 pip install tensorlayer
 pip install pillow
-git config --global user.name "Nick Steelman"\
+git config --global user.name "CleanestMink126"
 git config --global user.email "gns126@gmail.com"
