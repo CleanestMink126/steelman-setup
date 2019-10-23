33770f3f80f185171a47f126baefcce254aa6923
CleanestMink126 <gns126@gmail.com>
2019-10-22 20:35:15 -0400

    more deeping

--- setup.sh
+++ setup.sh
@@ -9,7 +9,9 @@ sudo apt install git
 sudo add-apt-repository ppa:webupd8team/atom
 sudo add-apt-repository ppa:leaeasy/dde
 sudo apt-get update
-apt-get install dde
+sudo apt-get install dde
+sudo apt install dde dde-file-manager
+sudo apt install deepin-gtk-theme
 sudo apt-get install atom
 sudo apt-get install python3
 pip install numpy
